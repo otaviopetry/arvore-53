dbb25ddfca7de308409117d1fe5b33f249392afc
Otavio Petry <otaviopetry@gmail.com>
2020-07-05 03:35:59 -0300

    feat: Profile screen Bookshelf and Tags
    Add Bookshelf with horizontal ScrollView and user tags

--- mobile/src/pages/Profile/index.js
+++ mobile/src/pages/Profile/index.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Image, TouchableOpacity, ScrollView } from 'react-native';
 import { Feather as Icon } from '@expo/vector-icons';
 import Constants from 'expo-constants';
 
 import AvatarPlaceholder from '../../assets/avatar-placeholder.png';
+import BookPlaceholder from '../../assets/placeholder-book-cover.png';
+import SmallBookPlaceholder from '../../assets/placeholder-smallbook.png';
 
 export default function Profile({ navigation }) {
 
@@ -19,86 +21,129 @@ export default function Profile({ navigation }) {
 
         <StatusBar style="light" backgroundColor="#45cbcd" />
 
-        { /* Profile Header: Avatar, Name, School and Badges */ }
-        <View style={styles.header}>
-
-            <View style={styles.linkGoBack}>
-                <TouchableOpacity onPress={ goBack }>
-                    <Icon name="arrow-left" size={20} />
-                </TouchableOpacity>
-            </View>
-            
-            <View style={styles.userContainer}>
-                <View style={styles.summary}>
-                    <View style={styles.avatarContainer}>
-                        <Image source={AvatarPlaceholder} style={styles.userAvatar} />
-                    </View>
-                    <View style={styles.userInfo}>
-                        <View>
-                            <Text style={styles.userName}>Bianca</Text>
-                            <Text style={styles.userTag}>Viajante do Tempo</Text>
-                            <Text style={styles.userText}>Colégio Leonardo da Vinci</Text>
-                            <Text style={styles.userText}>1º ano A - Médio</Text>
-                        </View>                  
-                    </View>
+        <ScrollView>
+            { /* Profile Header: Avatar, Name, School and Badges */ }
+            <View style={styles.header}>
+    
+                <View style={styles.linkGoBack}>
+                    <TouchableOpacity onPress={ goBack }>
+                        <Icon name="arrow-left" size={20} />
+                    </TouchableOpacity>
                 </View>
-            </View>
-        </View>
-
-        <View style={styles.badgesContainer}>
-            <View style={styles.badges}>
-                <Image style={styles.badge} source={{ uri: 'http://192.168.0.102:3333/journey/badges/badge-le-de-tudo.png' }} />
-                <Image style={styles.badge} source={{ uri: 'http://192.168.0.102:3333/journey/badges/badge-multicultural.png' }} />
-                <Image style={styles.badge} source={{ uri: 'http://192.168.0.102:3333/journey/badges/badge-100porcento-fiel.png' }} />
                 
-            </View>
-        </View>
-
-        { /* What I'm Reading section */ }
-        <View style={styles.whatImReading}>
-            <Text style={styles.sectionTitle}>O que estou lendo</Text>
-            <TouchableOpacity style={styles.theBook} onPress={ () => navigation.navigate('Book')}>
-                <View style={{width: '30%', marginRight: 16}}>
-                    <Image style={styles.bookCover} source={{ uri: 'http://192.168.0.102:3333/uploads/book-cover-Julio-Verne-01.jpeg' }} />
-                </View>
-                <View style={styles.theBook}>
-                    <View style={{flexDirection: 'column'}}>
-                        <Text style={styles.theBookTitle}>A volta ao mundo em 80 dias</Text>
-                        <Text style={styles.theBookAuthor}>Julio Verne</Text>
-                        <Text style={styles.theBookSynopsis}>Lorem Ipsum dolor sit amet consectetur adipiscing elit</Text>
+                <View style={styles.userContainer}>
+                    <View style={styles.summary}>
+                        <View style={styles.avatarContainer}>
+                            <Image source={AvatarPlaceholder} style={styles.userAvatar} />
+                        </View>
+                        <View style={styles.userInfo}>
+                            <View>
+                                <Text style={styles.userName}>Bianca</Text>
+                                <Text style={styles.userTag}>Viajante do Tempo</Text>
+                                <Text style={styles.userText}>Colégio Leonardo da Vinci</Text>
+                                <Text style={styles.userText}>1º ano A - Médio</Text>
+                            </View>                  
+                        </View>
                     </View>
+                </View>
+            </View>
+    
+            <TouchableOpacity onPress={ () => navigation.navigate('Achievements')}>
+                <View style={styles.badgesContainer}>
+                    
+                        <View style={styles.badges}>
+                            <Image style={styles.badge} source={{ uri: 'http://192.168.0.102:3333/journey/badges/badge-le-de-tudo.png' }} />
+                            <Image style={styles.badge} source={{ uri: 'http://192.168.0.102:3333/journey/badges/badge-multicultural.png' }} />
+                            <Image style={styles.badge} source={{ uri: 'http://192.168.0.102:3333/journey/badges/badge-100porcento-fiel.png' }} />
+                            
+                        </View>
                 </View>
             </TouchableOpacity>
-        </View>
 
-        { /* User Bookshelf */ }
-        <View style={styles.bookshelf}>
-            <Text>Minha estante</Text>
-            <View style={styles.theActualShelf}>
-                <Text>Livros da estante</Text>
-            </View>            
-        </View>
-
-        
-
-        { /* What I'm recommending section */ }
-        <View style={styles.whatImRecommending}>
-            <Text>O que eu indico</Text>
-            <View style={styles.recommendedBooksContainer}>
-                <View style={styles.recommendedBook}>
-                    <Text>Livro 1</Text>
-                    <Text>Sinopse do livro 1</Text>
-                </View>
-                <View style={styles.recommendedBook}>
-                    <Text>Livro 2</Text>
-                    <Text>Sinopse do livro 2</Text>
-                </View>
-                <View style={styles.recommendedBook}>
-                    <Text>Livro 3</Text>
-                    <Text>Sinopse do livro 3</Text>
+    
+            { /* What I'm Reading section */ }
+            <View style={styles.whatImReading}>
+                <Text style={styles.sectionTitle}>O que estou lendo</Text>
+                <TouchableOpacity style={styles.theBook} onPress={ () => navigation.navigate('Book')}>
+                    <View style={{width: '30%', marginRight: 16}}>
+                        <Image style={styles.bookCover} source={{ uri: 'http://192.168.0.102:3333/uploads/book-cover-Julio-Verne-01.jpeg' }} />
+                    </View>
+                    <View style={styles.theBook}>
+                        <View style={{flexDirection: 'column'}}>
+                            <Text style={styles.theBookTitle}>A volta ao mundo em 80 dias</Text>
+                            <Text style={styles.theBookAuthor}>Julio Verne</Text>
+                            <Text style={styles.theBookSynopsis}>Lorem Ipsum dolor sit amet consectetur adipiscing elit</Text>
+                        </View>
+                    </View>
+                </TouchableOpacity>
+            </View>
+    
+            { /* User Bookshelf */ }
+            <View style={styles.bookshelf}>
+                <Text style={styles.sectionTitle}>Minha estante</Text>
+                <Text style={styles.sectionSubtitle}>Livros que completei</Text>
+                <ScrollView style={styles.theActualShelf} horizontal>
+                    <View style={styles.book}>
+                        <Image source={SmallBookPlaceholder} />
+                    </View>
+                    <View style={styles.book}>
+                        <Image source={SmallBookPlaceholder} />
+                    </View>
+                    <View style={styles.book}>
+                        <Image source={SmallBookPlaceholder} />
+                    </View>
+                    <View style={styles.book}>
+                        <Image source={SmallBookPlaceholder} />
+                    </View>
+                    <View style={styles.book}>
+                        <Image source={SmallBookPlaceholder} />
+                    </View>
+                    <View style={styles.book}>
+                        <Image source={SmallBookPlaceholder} />
+                    </View>
+                    <View style={styles.book}>
+                        <Image source={SmallBookPlaceholder} />
+                    </View>
+                    <View style={styles.book}>
+                        <Image source={SmallBookPlaceholder} />
+                    </View>
+                </ScrollView>            
+            </View>
+    
+            
+    
+            { /* What I'm recommending section */ }
+            <View style={styles.whatImRecommending}>
+                <Text style={styles.sectionTitle}>O que eu indico</Text>
+                <Text style={styles.sectionSubtitle}>Livros que me marcaram</Text>
+                <View style={styles.recommendedBooksContainer}>
+                    <View style={styles.recommendedBook}>
+                        <Image style={{width: 120}} source={BookPlaceholder} />
+                    </View>
+                    <View style={styles.recommendedBook}>
+                        <Image style={{width: 120}} source={BookPlaceholder} />
+                    </View>
+                    <View style={styles.recommendedBook}>
+                        <Image style={{width: 120}} source={BookPlaceholder} />
+                    </View>
+                    
                 </View>
             </View>
-        </View>
+            
+            { /* User reading habits tags */ }
+            <View style={styles.tags}>
+                <Text style={styles.sectionTitle}>Tags de interesse</Text>
+                <View style={styles.tagContainer}>
+                    <Text style={styles.tag}>Aventura</Text>                  
+                    <Text style={styles.tag}>História</Text>                  
+                    <Text style={styles.tag}>Ciências</Text>                  
+                    <Text style={styles.tag}>Suspense</Text>                  
+                    <Text style={styles.tag}>Autores antigos</Text>                  
+                </View>                           
+            </View>
+
+            
+        </ScrollView>
         
     </SafeAreaView>
     );
@@ -109,7 +154,7 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         flex: 1,
         paddingTop: Constants.statusBarHeight,
-        backgroundColor: '#eee'
+        backgroundColor: '#e1efef'
     },
     bookCover: {
         width: '100%',
@@ -118,6 +163,11 @@ const styles = StyleSheet.create({
     sectionTitle: {
         fontWeight: 'bold',
         fontSize: 16
+    },
+    sectionSubtitle: {
+        marginBottom: 8,
+        fontSize: 13,
+        fontStyle: 'italic'
     },
     mainText: {
         marginTop: 8,
@@ -183,7 +233,10 @@ const styles = StyleSheet.create({
         marginHorizontal: 8
     },
     bookshelf: {
-        padding: 2
+        padding: 16
+    },
+    book: {
+        marginRight: 8
     },
     theActualShelf: {
 
@@ -193,21 +246,24 @@ const styles = StyleSheet.create({
         paddingVertical: 16
     },
     theBook: {
-        marginTop: 8,
         flexDirection: 'row',
-        flexShrink: 1
+        flexShrink: 1,
+        marginTop: 8
     },
     theBookTitle: {
         fontSize: 24,
         color: '#006266',
         fontWeight: 'bold'
     },
+    theBookAuthor: {
+        fontWeight: 'bold'
+    },
     theBookSynopsis: {
         flexShrink: 1
     },
     whatImRecommending: {
-        borderWidth: 1,
-        padding: 2
+        padding: 16,
+        backgroundColor: '#eee'        
     },
     recommendedBooksContainer: {
         flexDirection: 'row',
@@ -215,5 +271,24 @@ const styles = StyleSheet.create({
     },
     recommendedBook: {
         marginRight: 8
+    },
+    tags: {
+        padding: 16
+    },
+    tagContainer: {
+        marginTop: 8,
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+    },
+    tag: {
+        paddingHorizontal: 14,
+        paddingVertical: 8,
+        backgroundColor: '#fff',
+        color: '#006266',
+        marginRight: 8,
+        marginBottom: 8,
+        borderRadius: 16,
+        flexWrap: 'wrap',
+        textTransform: 'lowercase'
     }
 });
