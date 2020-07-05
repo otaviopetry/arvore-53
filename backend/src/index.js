b230910477d550fa5aca684cd8ae148bc0ab329d
Otavio Petry <otaviopetry@gmail.com>
2020-07-05 02:37:56 -0300

    feat: Badges and What Im Reading
    Several changes in Profile Screen, adding Badges and Featured Book

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -8,5 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use('/journey/badges', express.static(path.resolve(__dirname, '..', 'journey/badges')));
 
 app.listen(3333);
--- mobile/src/pages/Profile/index.js
+++ mobile/src/pages/Profile/index.js
@@ -17,30 +17,58 @@ export default function Profile({ navigation }) {
     /* Big container */
     <SafeAreaView style={styles.container}>
 
-        <TouchableOpacity onPress={ goBack }>
-            <Icon name="arrow-left" size={24} />
-        </TouchableOpacity>
+        <StatusBar style="light" backgroundColor="#45cbcd" />
 
-        { /* Profile Header: Avatar, Name, School and Badges */ } 
+        { /* Profile Header: Avatar, Name, School and Badges */ }
         <View style={styles.header}>
+
+            <View style={styles.linkGoBack}>
+                <TouchableOpacity onPress={ goBack }>
+                    <Icon name="arrow-left" size={20} />
+                </TouchableOpacity>
+            </View>
             
-            <View style={styles.summary}>
-                <Image source={AvatarPlaceholder} style={styles.userAvatar} />
-                <View style={styles.userInfo}>
-                    <View>
-                        <Text>Bianca</Text>
-                        <Text>1º ano - Colégio Santa Maria</Text>
+            <View style={styles.userContainer}>
+                <View style={styles.summary}>
+                    <View style={styles.avatarContainer}>
+                        <Image source={AvatarPlaceholder} style={styles.userAvatar} />
                     </View>
-                    <View style={styles.badgeContainer}>
-                        <Icon name="alert-octagon" style={styles.icon} />
-                        <Icon name="anchor" style={styles.icon} />
-                        <Icon name="archive" style={styles.icon} />
+                    <View style={styles.userInfo}>
+                        <View>
+                            <Text style={styles.userName}>Bianca</Text>
+                            <Text style={styles.userTag}>Viajante do Tempo</Text>
+                            <Text style={styles.userText}>Colégio Leonardo da Vinci</Text>
+                            <Text style={styles.userText}>1º ano A - Médio</Text>
+                        </View>                  
                     </View>
-                    <TouchableOpacity onPress={ () => navigation.navigate('Achievements')}>
-                        <Text>Ver conquistas</Text>
-                    </TouchableOpacity>                    
                 </View>
             </View>
+        </View>
+
+        <View style={styles.badgesContainer}>
+            <View style={styles.badges}>
+                <Image style={styles.badge} source={{ uri: 'http://192.168.0.102:3333/journey/badges/badge-le-de-tudo.png' }} />
+                <Image style={styles.badge} source={{ uri: 'http://192.168.0.102:3333/journey/badges/badge-multicultural.png' }} />
+                <Image style={styles.badge} source={{ uri: 'http://192.168.0.102:3333/journey/badges/badge-100porcento-fiel.png' }} />
+                
+            </View>
+        </View>
+
+        { /* What I'm Reading section */ }
+        <View style={styles.whatImReading}>
+            <Text style={styles.sectionTitle}>O que estou lendo</Text>
+            <TouchableOpacity style={styles.theBook} onPress={ () => navigation.navigate('Book')}>
+                <View style={{width: '30%', marginRight: 16}}>
+                    <Image style={styles.bookCover} source={{ uri: 'http://192.168.0.102:3333/uploads/book-cover-Julio-Verne-01.jpeg' }} />
+                </View>
+                <View style={styles.theBook}>
+                    <View style={{flexDirection: 'column'}}>
+                        <Text style={styles.theBookTitle}>A volta ao mundo em 80 dias</Text>
+                        <Text style={styles.theBookAuthor}>Julio Verne</Text>
+                        <Text style={styles.theBookSynopsis}>Lorem Ipsum dolor sit amet consectetur adipiscing elit</Text>
+                    </View>
+                </View>
+            </TouchableOpacity>
         </View>
 
         { /* User Bookshelf */ }
@@ -51,19 +79,7 @@ export default function Profile({ navigation }) {
             </View>            
         </View>
 
-        { /* What I'm Reading section */ }
-        <View style={styles.whatImReading}>
-            <Text>O que estou lendo</Text>
-            <TouchableOpacity style={styles.theBook} onPress={ () => navigation.navigate('Book')}>
-                <Image source={AvatarPlaceholder} />
-                <View style={styles.theBook}>
-                    <View>
-                        <Text style={styles.theBookTitle}>A volta ao mundo em 80 dias</Text>
-                        <Text style={styles.theBookAuthor}>Julio Verne</Text>
-                    </View>
-                </View>
-            </TouchableOpacity>
-        </View>
+        
 
         { /* What I'm recommending section */ }
         <View style={styles.whatImRecommending}>
@@ -92,50 +108,102 @@ const styles = StyleSheet.create({
     container: {
         borderWidth: 1,
         flex: 1,
-        padding: 8,
-        paddingTop: Constants.statusBarHeight + 20,
-        backgroundColor: '#45cbcd',
-        },
-        logoImg: {
-            marginBottom: 16
-        },
-        mainText: {
-            marginTop: 8,
-            fontWeight: 'bold',
-            color: '#fff'
+        paddingTop: Constants.statusBarHeight,
+        backgroundColor: '#eee'
+    },
+    bookCover: {
+        width: '100%',
+        height: 200
+    },
+    sectionTitle: {
+        fontWeight: 'bold',
+        fontSize: 16
+    },
+    mainText: {
+        marginTop: 8,
+        fontWeight: 'bold',
+        color: '#fff'
     },
     header: {
-        borderWidth: 1,
-        alignItems: 'center',
-        justifyContent: 'space-around',
-        padding: 8,
+        backgroundColor: '#c7ecee',
+        paddingBottom: 64
+    },
+    linkGoBack: {
+        marginTop: 16,
+        marginLeft: 8
+    },
+    avatarContainer: {
+        marginLeft: 16
+    },
+    userContainer: {
+        alignItems: 'stretch',
         marginTop: 16,        
         flexDirection: 'row'
-        
+    },
+    userName: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#006266'
+    },
+    userTag: {
+        fontSize: 16,
+        color: '#006266',
+        fontStyle: 'italic',
+        marginBottom: 8
     },
     summary: {
         flexDirection: 'row'
     },
     userAvatar: {
-        marginRight: 8
+        marginRight: 16
     },
-    badgeContainer: {
-        flexDirection: 'row'
+    badgesContainer: {
+        alignItems: 'center',
+        marginBottom: 16
+    },
+    badges: {
+        marginTop: -24,
+        marginBottom: 16,
+        paddingHorizontal: 16,
+        height: 48,
+        backgroundColor: '#55adc1',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center',
+        borderRadius: 40
+    },
+    badge: {
+        width: 96,
+        height: 96,
+        marginHorizontal: 8
+    },
+    icon: {
+        fontSize: 36,
+        marginHorizontal: 8
     },
     bookshelf: {
-        borderWidth: 1,
         padding: 2
     },
     theActualShelf: {
 
     },
     whatImReading: {
-        borderWidth: 1,
-        padding: 2
+        paddingHorizontal: 16,
+        paddingVertical: 16
     },
     theBook: {
         marginTop: 8,
-        flexDirection: 'row'
+        flexDirection: 'row',
+        flexShrink: 1
+    },
+    theBookTitle: {
+        fontSize: 24,
+        color: '#006266',
+        fontWeight: 'bold'
+    },
+    theBookSynopsis: {
+        flexShrink: 1
     },
     whatImRecommending: {
         borderWidth: 1,
